e7f7a9f4d5e96a171be77bcba87f618809064b68
Piruin Panichphol <piruin.p@gmail.com>
2021-09-26 16:10:49 +0700

    Remove `MultiGeometry`

--- geok/src/commonMain/kotlin/me/piruin/geok/geometry/MultiGeometry.kt
+++
@@ -1,9 +0,0 @@
-package me.piruin.geok.geometry
-
-import me.piruin.geok.BBox
-
-@Deprecated("Replace with GeometryCollection", ReplaceWith("GeometryCollection<*>"))
-interface MultiGeometry : Geometry {
-
-    val bbox: BBox
-}
